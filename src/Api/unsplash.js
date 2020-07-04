ec0bce8b2339d0d5f8a0e359fd31a62dc92bd2d3
Lawrence <Caulker>
2020-07-04 18:04:40 -0400

    more stuff

---
+++ src/Api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+export default axios.create({
+  // baseURl: "https://api.unsplash.com",
+  headers: {
+    Authorization: "Client-ID yZs0ZCejr8fYCOKf1N3wjQ9drmRkQ-AfN9IQTS9gZ28",
+  },
+})
---
+++ src/components/ImageCard.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from "react"
+
+class ImageCard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { spans: 0 }
+
+    this.imageRef = React.createRef()
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef.current.addEventListener("load", this.setSpans))
+  }
+
+  setSpans = () => {
+    const height = console.log(this.imageRef.current.clientHeight)
+    const spans = Math.ceil(height / 100)
+
+    this.setState({
+      spans,
+    })
+  }
+
+  render() {
+    const { urls, description } = this.props.image
+
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
+      </div>
+    )
+  }
+}
+
+export default ImageCard
