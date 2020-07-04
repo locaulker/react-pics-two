08fb1bc61a993b0ace3470156327c6ffb9e7c147
Lawrence <Caulker>
2020-07-04 00:41:13 -0400

    the "this" keyword

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react"
 
 class SearchBar extends Component {
-  onInputChange(event) {
-    console.log(event.target.value)
+  state = { term: "" }
+
+  onFormSubmit = event => {
+    event.preventDefault()
+
+    console.log(this.state.term)
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label htmlFor="">Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
